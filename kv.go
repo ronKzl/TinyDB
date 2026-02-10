b6067a41aa0e790ce65135d758bdf707ea078fab
RonKzl <ron.stuchevsky@gmail.com>
2026-02-10 17:32:35 -0500

    refactored KV to use sorted slices under the hood

--- kv.go
+++ kv.go
@@ -8,14 +8,12 @@ import (
 
 type KV struct {
 	log Log
-	mem map[string][]byte // TODO: Remove
-	// * Refactoring from map to slices so I can implement comparison operations later
 	// * future note: currently all of the DB data needs to be loaded into memory ~ need to read up on ways to only have partial memory loaded
 	keys [][]byte
-	vals [][]byte 
+	vals [][]byte
 }
 
-type updateMode int 
+type updateMode int
 
 const (
 	ModeUpsert updateMode = 0 // insert OR update
@@ -24,74 +22,99 @@ const (
 )
 
 func (kv *KV) Open() error {
-	// TODO: Rework
+
 	if err := kv.log.Open(); err != nil {
 		return err
 	}
-	kv.mem = map[string][]byte{}
-	
+	// neat trick to reuse existing memory
+	kv.keys = kv.keys[:0]
+	kv.vals = kv.vals[:0]
+
+	entries := []Entry{}
+
 	for {
 		entry := Entry{}
 		eof, err := kv.log.Read(&entry)
-		
-		if eof || err == ErrBadSum || err == io.ErrUnexpectedEOF { break }
-		if err != nil { return err}
-		
-		// entry go created/updated OR deleted
-		if entry.deleted {
-			delete(kv.mem, string(entry.key))
-		} else {
-			kv.mem[string(entry.key)] = entry.val
+
+		if eof || err == ErrBadSum || err == io.ErrUnexpectedEOF {
+			break
 		}
+		if err != nil {
+			return err
+		}
+
+		entries = append(entries, entry)
 	}
 
+	// groups together the same key operations
+	slices.SortStableFunc(entries, func(a Entry, b Entry) int {
+		return bytes.Compare(a.key, b.key)
+	})
+
+	for _, entry := range entries {
+		n := len(kv.keys)
+		if n > 0 && bytes.Equal(entry.key, kv.keys[n-1]) {
+			//remove current entry found for the key found an updated one
+			kv.keys = kv.keys[:n-1]
+			kv.vals = kv.vals[:n-1]
+		}
+
+		if !entry.deleted {
+			kv.keys = append(kv.keys, entry.key)
+			kv.vals = append(kv.vals, entry.val)
+		}
+	}
 	return nil
 }
 
 func (kv *KV) Close() error { return kv.log.Close() }
 
 func (kv *KV) Get(key []byte) (val []byte, ok bool, err error) {
-	if idx, found := BinarySearchFunc(kv.keys,key,bytes.Compare); found {
+	if idx, found := BinarySearchFunc(kv.keys, key, bytes.Compare); found {
 		return kv.vals[idx], found, nil
 	}
-	return nil, false, nil 
+	return nil, false, nil
 }
 
 func (kv *KV) Set(key []byte, val []byte) (updated bool, err error) {
-	return kv.SetEx(key,val,ModeUpsert)
+	return kv.SetEx(key, val, ModeUpsert)
 }
 
 func (kv *KV) Del(key []byte) (deleted bool, err error) {
-	if idx, found := BinarySearchFunc(kv.keys,key,bytes.Compare); found {
+	if idx, found := BinarySearchFunc(kv.keys, key, bytes.Compare); found {
 		if err = kv.log.Write(&Entry{key: key, deleted: true}); err != nil {
-			return false, err 
+			return false, err
 		}
-		kv.keys = slices.Delete(kv.keys,idx,idx+1)
-		kv.vals = slices.Delete(kv.vals,idx,idx+1)
-		return true, nil 
+		kv.keys = slices.Delete(kv.keys, idx, idx+1)
+		kv.vals = slices.Delete(kv.vals, idx, idx+1)
+		return true, nil
 	}
 	return false, nil
 }
 
 func (kv *KV) SetEx(key []byte, val []byte, mode updateMode) (updating bool, err error) {
-	idx, existed := BinarySearchFunc(kv.keys,key,bytes.Compare)
-	// TODO: Rework
-	prevVal, existed := kv.mem[string(key)]
-	switch mode{
+	idx, existed := BinarySearchFunc(kv.keys, key, bytes.Compare)
+
+	switch mode {
 	case ModeUpsert:
-		updating = !existed || !bytes.Equal(prevVal,val)
+		updating = !existed || !bytes.Equal(kv.vals[idx], val)
 	case ModeInsert:
 		updating = !existed
 	case ModeUpdate:
-		updating = existed && !bytes.Equal(prevVal,val)
+		updating = existed && !bytes.Equal(kv.vals[idx], val)
 	default:
 		panic("NOT A VALID UPDATE MODE")
 	}
 	if updating {
-			if err := kv.log.Write(&Entry{key: key,val: val, deleted: false}); err != nil {
-				return false, err 
-			}
-			kv.mem[string(key)] = val
+		if err := kv.log.Write(&Entry{key: key, val: val, deleted: false}); err != nil {
+			return false, err
+		}
+		if existed {
+			kv.vals[idx] = val
+		} else {
+			kv.keys = slices.Insert(kv.keys, idx, key)
+			kv.vals = slices.Insert(kv.vals, idx, val)
+		}
 	}
 	return updating, nil
 }
